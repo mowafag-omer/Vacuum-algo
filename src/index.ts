de050f7f314ff7a98e6e43a4ed6a3e25f963a3f4
Mowafag <mmowfug@yahoo.com>
2021-05-10 19:09:53 +0200

    readme update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import Vacuum from './vacuum'
 
+
+/* vacuum1 is an instance of the class Vacuum, 
+it takes arguments two arrays, the dimensions [x, y] 
+initial position [x, y, orientation]  */
+
+/* vacuum1.executeInstructions takes the Instructions as argument, executes them 
+and communicate the results by appending them in index.html */
+
 (() => {
-  const vacuum1:Vacuum = new Vacuum([10, 10], [5, 5, 'N'])
-  vacuum1.executeInstructions('DADADADAAD')
+  const vacuum1: Vacuum = new Vacuum([10, 10], [5, 5, 'N'])
+  vacuum1.executeInstructions('DADADADAA')
 })()
 
